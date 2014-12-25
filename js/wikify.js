3de3d881758965a817d0f7a03d206d70566bf01a
Joachim Happel <joachim.happel@gmail.com>
2014-12-25 16:00:14 +0100

    initial

---
+++ js/wikify.js
@@ -0,0 +1,251 @@
+jQuery(document).ready(function($) {
+	
+
+
+    tinymce.create('tinymce.plugins.wikify_plugin', {
+		maxId:0,
+		wikiwords:{},
+		activeNode:null,		
+		
+		getWikiWords: function(el){
+		
+			
+		
+			var ids = [], ww;
+			
+			ww = el.getElementsByClassName('linkword');
+			
+			
+			for(i=0;i<ww.length;i++){
+				var splitted = ww[i].innerHTML.split('|');
+				var newword = splitted[0].trim();
+				if(!this.wikiwords[ww[i].id]){
+					this.wikiwords[ww[i].id] = {};
+				}
+				this.wikiwords[ww[i].id].label=newword;
+				ids.push(newword);
+				
+				if(this.wikiwords[ww[i].id].checked){
+					
+					if(this.wikiwords[ww[i].id].exists){
+						ww[i].className = 'wikify linkword existing-wikipost';
+					}else{
+						ww[i].className = 'wikify linkword';
+					}
+				}
+				if(this.wikiwords[ww[i].id].slug){
+					ww[i].href = this.wikiwords[ww[i].id].slug;
+				}
+			}
+			if(ids.length){
+				if	(	(!this.wikiwords.all) || 
+						(ids!==this.wikiwords.all)
+					){
+						this.wikiwords.all =  ids;
+						console.info('checkserver'); 
+						
+						
+						
+						this.checkWikiWords();
+						
+						
+				}
+			}
+						
+			
+		},
+		setWikiWordStyles: function(){
+			var c = tinyMCE.activeEditor.getContent();
+			var i=0;
+			c = c.replace(/\[\[([^\]]+)\]\]/g, function(match, contents){
+				i++;
+				contents = contents.replace(/<[^>]*>/g,'');
+				contents = contents.replace(/[\[\]]/g,'').trim();
+				var parts = contents.split('|');
+				if(parts.length==1){
+					bracket = "[[<a href=\"#\" id=\"wikiword-"+i+"\" class=\"wikify linkword\">"+parts[0].trim()+"</a>]]";
+				}else{
+					bracket = "[[<a href=\"#\" id=\"wikiword-"+i+"\" class=\"wikify linkword\">"+parts[0].trim()+"</a>|<span class=\"wikify linklabel\">"+ parts[1].trim() +"</span>]]";
+				}
+				return bracket;
+			});
+			this.maxId = i;
+			tinyMCE.activeEditor.setContent(c);
+			
+			
+			
+		},
+		checkWikiWords: function(){
+			
+			var p=this, abord = true;
+			
+			for(elem in p.wikiwords){
+				if(elem == 'all') continue;
+				if(!p.wikiwords[elem].checked) {
+					abord = false;
+					break;
+				}
+			}
+			if(abord) return;
+			
+			$.ajax({
+				url: ajaxurl,
+				data: {
+					'action':'wikify_post_exists',
+					'contents' : p.wikiwords.all.join()
+				},
+				success:function(data) {
+					data = jQuery.parseJSON(data);
+					if(data.result){
+						//reset
+						for(elem in p.wikiwords){
+							if(elem == 'all') continue;
+							p.wikiwords[elem].exists = false;
+						}
+						//compare with serverdata
+						for(i=0;i<data.result.length;i++){
+							var obj = data.result[i];
+							
+							
+							for(elem in p.wikiwords){
+								
+								if(elem == 'all') continue;
+								
+								if(obj.label == p.wikiwords[elem].label){
+									p.wikiwords[elem].checked = true;
+									if(obj.slug){
+										//console.log(obj.slug)
+										p.wikiwords[elem].exists = true;
+										p.wikiwords[elem].slug = obj.slug;
+									}else{
+										p.wikiwords[elem].slug = '/wp-admin/post-new.php?post_title='+obj.label;
+									}
+									
+								}
+								
+								
+							}
+						}
+						//console.log(p.wikiwords);
+						p.getWikiWords(p.ed.dom.doc);
+					}
+					
+				},
+				error: function(errorThrown){
+					//console.log(errorThrown);
+				}
+			});
+		},
+		
+		init : function(ed, url) {
+		
+			this.ed = ed;
+			
+			var p = this;
+			
+						
+			ed.on('change',function(e){
+				p.getWikiWords(e.target.dom.doc);
+				
+			});
+			//removes wikiword styles
+			tinyMCE.activeEditor.on('GetContent',function(e){
+				e.content = e.content.replace(/(\[\[[^\]]+\]\])/g, function(match, contents){
+					return 	contents.replace(/<[^>]*>/g,'');
+				});
+			});
+			
+			//mask wikiwords onLoad
+			ed.on('LoadContent',function(e){
+				p.setWikiWordStyles();
+				p.wikiwords={},
+				p.getWikiWords(ed.dom.doc);
+				
+			});
+			
+			
+			// Register command for when button is clicked
+			ed.addCommand('wikify_insert_brackets', function() {
+				
+				if($(tinyMCE.activeEditor.selection.getNode()).hasClass('wikify')) return;
+				
+				selected = tinyMCE.activeEditor.selection.getContent();
+				
+				
+				if( selected ){
+					p.maxId ++;
+					//If text is selected when button is clicked
+					//Wrap brackets around it.
+					content =  '[[<a href="#" id="wikiword-'+p.maxId+'" class="wikify linkword">'+selected.trim()+'</a>]]';
+					if(selected.substring(selected.length-1) == ' ') content += ' ';
+					
+					p.getWikiWords(ed.dom.doc);
+					
+					
+					
+				}else{
+					
+					content =  '';
+										
+					
+				}
+
+				tinymce.execCommand('mceInsertContent', false, content);
+				
+					
+			});
+			
+			// Register command for when button is clicked
+			ed.addCommand('wikify_insert_brackets_label', function() {
+			
+				if($(tinyMCE.activeEditor.selection.getNode()).hasClass('wikify')) return;
+				
+				selected = tinyMCE.activeEditor.selection.getContent();
+
+				if( selected ){
+					p.maxId ++;
+					//If text is selected when button is clicked
+					//Wrap brackets around it.
+					
+					var wikiword = prompt(tinyMCE.activeEditor.getParam('wikify_lang_prompt'));
+					if(wikiword == null){
+					
+						return;
+					
+					}else{
+						
+						content =  '[[<a href="/'+wikiword+'" id="wikiword-'+p.maxId+'"  class="wikify linkword">'+wikiword+'</a>|<span class="wikify linklabel">'+selected.trim()+'</span>]]';
+						if(selected.substring(selected.length-1) == ' ') content += ' ';
+						
+						p.getWikiWords(ed.dom.doc);
+					
+					}
+					
+				}else{
+					
+					content =  '';
+										
+					
+				}
+
+				tinymce.execCommand('mceInsertContent', false, content);
+				
+					
+			});
+			
+            // Register buttons - trigger above command when clicked
+            ed.addButton('wikify_button', {title : 'Wikify', cmd : 'wikify_insert_brackets', image: url + '/icons/wikify.png' });
+			ed.addButton('wikify_split_button', {title : 'Wikify Label', cmd : 'wikify_insert_brackets_label', image: url + '/icons/wikilabel.png' });
+			ed.on('init', function() {
+				ed.execCommand('wikify_content', false, null, {skip_focus: true});
+			});
+			
+		}});
+           
+    
+
+    // Register our TinyMCE plugin
+    // first parameter is the button ID
+    // second parameter must match the first parameter of the tinymce.create() function above
+    tinymce.PluginManager.add('wikify_button', tinymce.plugins.wikify_plugin);
+});
---
+++ wikify.php
@@ -0,0 +1,191 @@
+<?php
+/*
+  Plugin Name: Wikify
+  Plugin URI: http://www.joachim-happel.de/wikify
+  Description: An easy-to-use plugin that adds the functionality of a wiki known from MediaWiki. It handles [[word]] and [[word|link label]] .
+  Version: 0.2
+  Author: Joachim Happel
+  Author URI: http://www.joachim-happel.de
+
+  Features:
+  * generate an auto-link to word in double brackets to a Post, with this link
+  * handles [[word]] and [[word|label]] 
+  * 
+  
+  **ATTENTION** 
+  
+  Works only with this permalink settings:  /%postname%/
+  
+    
+    
+ */
+ 
+class wp_wikify {
+
+	public $forbiddenSlugs = array("login");
+	public $urlPrefix;
+	
+
+	function __construct() {
+		ini_set("display_errors", 'on');
+		ini_set('display_errors',1);
+		ini_set('display_startup_errors',1);
+		ini_set('error_reporting', E_ERROR);
+		//error_reporting(-1);
+
+		$this->urlPrefix = get_home_url() . '/';
+
+		add_filter('the_content', array($this, 'wikifyContent'));
+	
+		add_action('init',array($this,'wikify_button_init') );
+		
+		
+	}
+
+	
+	//Add a wikfy Button to the TinyMCE Editor
+	function wikify_button_init() {
+
+		//Abort early if the user will never see TinyMCE
+		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') && get_user_option('rich_editing') == 'true')
+		   return;
+
+		//Add a callback to regiser our tinymce plugin   
+		add_filter("mce_external_plugins",array($this, "wikify_register_tinymce_plugin") ); 
+
+		// Add a callback to add our button to the TinyMCE toolbar
+		add_filter('mce_buttons', array($this,'wikify_add_tinymce_button') );
+
+		// Add a callback to add our stylesheet to mark the brackets
+		add_filter( 'tiny_mce_before_init',  array($this,'wikify_add_tinymce_settings') );
+		
+	}
+
+	//a callback to add a button to the toolbar
+	function wikify_add_tinymce_button($buttons) {
+		//Add the button ID to the $button array
+		$buttons[] = "wikify_button";
+		$buttons[] = "wikify_split_button";
+		return $buttons;
+	}	
+	
+	//a callback to register the wikify plugin
+	function wikify_register_tinymce_plugin($plugin_array) {
+		$plugin_array['wikify_button'] = plugins_url('js/wikify.js', __FILE__) ;
+		return $plugin_array;
+	}
+	
+	//a callback to add a editor stylesheet
+	function wikify_add_tinymce_settings( $initSettings )
+	{
+			$initSettings['content_css'] .= ','.plugins_url('css/editor-style.css', __FILE__);
+			
+			$initSettings['wikify_lang_prompt'] = __('Which term should be linked?', 'wikify');
+			
+			return $initSettings;
+	}
+
+	/**
+	* find [[ ]] enclosed words
+	* either [[word]] or [[word|label]] 
+	* and send matches to wikifyBracketedElement
+	*/
+	function wikifyContent($theContent) {
+		
+		return preg_replace_callback("/\[\[([^\]]+)\]\]/", array($this, 'wikifyBracketedElement'), $theContent);
+		
+	}
+
+	// split word|label and sends it to wikiLink
+	function wikifyBracketedElement($matches) {
+		
+		$splitted = explode('|',$matches[1]);
+		
+		$wikiword = $label = trim($splitted[0]);
+				
+		if (isset($splitted[1])){
+			$label = trim($splitted[1]);
+		}
+		
+		return $this->wikiLink($wikiword, $label);
+		
+	}
+
+	// returns a link from a wiki word (wikiword|label)
+	// if post not exists, the wikiword will be marked as undefined and
+	// linked to a new post to define the wikiword
+	function wikiLink($wikiword, $label) {
+	
+		$class = $href = '';
+		$slug = sanitize_title(strip_tags($wikiword));
+		
+		if(in_array($slug, $this->forbiddenSlugs)) return $label; ;
+	
+		if(!self::postExists($slug)){
+			return  '<a title="'.__('Do explain', 'wikify').' '.$wikiword.'" href="' . $this->urlPrefix . "wp-admin/post-new.php?post_title=".urlencode($wikiword) . '" style="color:inherit">'.
+					'<span style="border-bottom: .5px dashed;">' . $label . '</span></a>';
+		}
+		
+		return '<a title="relilex: '.$label.'" href="' . $this->urlPrefix .$slug. '">' . $label . '</a>';
+	}
+	
+	
+
+	static function postExists($slug){
+		global $wpdb;
+		$rs = $wpdb->get_var( 
+			$wpdb->prepare( 
+				"SELECT count(ID) FROM $wpdb->posts
+				 WHERE post_name = %s
+				", $slug 
+			)
+		);
+		return ( $rs>0) ? true : false;
+	}
+}
+$wikify_instance = new wp_wikify();
+
+
+
+function wikify_post_exists(){
+	global $wpdb;
+	$contents = explode(',',$_REQUEST['contents']);
+	$wikiwords = array(); $data=$result= array();
+	
+	foreach($contents as $c){
+		$slug = esc_sql( sanitize_title(strip_tags($c)) );
+		$wikiwords[] = $slug;
+		$data[$slug]['label'] = $c;
+	}
+	$in ="'". implode("','", $wikiwords) ."'";
+	$rs = $wpdb->get_results( 
+		
+			"SELECT post_name, post_title FROM $wpdb->posts
+			 WHERE post_name in ($in )
+			" 
+		
+	);
+	if($rs ){
+		foreach($rs as $ds){
+			$data[$ds->post_name]['title'] = $ds->post_title;
+			$data[$ds->post_name]['slug']	= $ds->post_name;
+		}
+		foreach($data as $d){
+			$result[]=$d;
+		}
+	}
+	echo json_encode(array('result' =>  $result) );
+	die();
+	$slug = sanitize_title(strip_tags($_REQUEST['slug']));
+	if(wp_wikify::postExists($slug)){
+		echo json_encode(array('exists'=> true , 'slug'=> $slug ,'label' => $_REQUEST['slug'] ));
+	}else{
+		echo json_encode(  array('exists'=> false,
+								 'slug'=>$slug,
+								 'label' => $_REQUEST['slug'] ));
+	}
+	die();
+}
+add_action( 'wp_ajax_wikify_post_exists', 'wikify_post_exists');
+
+?>
